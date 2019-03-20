b0b890e2d827656cc9eabc2620b0c7db338b808c
erik <zerpaerik@gmail.com>
2019-03-20 09:40:25 -0430

    search atenciones

--- resources/views/movimientos/atenciones/search.blade.php
+++ resources/views/movimientos/atenciones/search.blade.php
@@ -44,6 +44,7 @@
 							<th>Monto</th>
 							<th>Monto Abonado</th>
 							<th>Fecha</th>
+														<th>Registrado Por:</th>
 							<th>Acciones</th>
 							<th></th>
 						</tr>
@@ -64,6 +65,7 @@
 						<td>{{$d->monto}}</td>
 						<td>{{$d->abono}}</td>
 						<td>{{$d->created_at}}</td>
+					<td>{{$d->user}},{{$d->userp}}</td>
 		                <td><a target="_blank" class="btn btn-primary" href="{{$model1.'-ver-'.$d->id}}">Ver Ticket</a></td>	
 						      @if(\Auth::user()->role_id <> 6)							 
 											<td><a class="btn btn-warning" href="{{$model . '-edit-' .$d->id}}">Editar</a></td>
