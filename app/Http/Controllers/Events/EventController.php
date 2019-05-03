6e288e1e615eba6382f2fb7f8504aae4e729b0d3
erik <zerpaerik@gmail.com>
2019-05-03 18:53:26 -0430

    ccc

--- app/Http/Controllers/Events/EventController.php
+++ app/Http/Controllers/Events/EventController.php
@@ -114,10 +114,9 @@ class EventController extends Controller
   {
   
     $paciente = DB::table('events as e')
-    ->select('e.id as EventId','e.paciente','e.title','e.profesional','e.date','e.monto','e.time','p.dni','p.direccion','p.telefono','p.fechanac','p.gradoinstruccion','p.ocupacion','p.nombres','p.apellidos','p.id as pacienteId','per.name as nombrePro','per.lastname as apellidoPro','per.id as profesionalId','rg.start_time','rg.end_time','rg.id')
+    ->select('e.id as EventId','e.paciente','e.title','e.profesional','e.date','e.monto','e.time','p.dni','p.direccion','p.telefono','p.fechanac','p.gradoinstruccion','p.ocupacion','p.nombres','p.apellidos','p.id as pacienteId','per.name as nombrePro','per.lastname as apellidoPro','per.id as profesionalId')
     ->join('pacientes as p','p.id','=','e.paciente')
     ->join('personals as per','per.id','=','e.profesional')
-    ->join('rangoconsultas as rg','rg.id','=','e.time')
     ->where('e.id','=',$id)
     ->first();
 
