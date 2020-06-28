f2c81f3508530dd7ef637430335e4311d566ced2
zerpaerik <zerpaerik@gmail.com>
2020-06-28 19:17:20 -0400

    Create general_detalleg.blade.php

---
+++ resources/views/reportes/general_detalleg.blade.php
@@ -0,0 +1,20 @@
+ 
+@extends('layouts.app')
+
+@section('content')
+	<div class="row">
+		<div class="col-xs-12">
+			<form action="reporte/detalladog" method="get">
+				<div class="form-group">
+					<div class="col-xs-6">
+						<label class="control-label">Fecha a solictar</label>
+						<input type="date" name="f1"  style="line-height: 20px">
+						<input type="date" name="f2"  style="line-height: 20px">
+						<button target="_blank" type="submit">Generar Reporte</button>
+						{{ csrf_field() }}
+					</div>
+				</div>
+			</form>
+		</div>
+	</div>
+@endsection
