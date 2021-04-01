94453280b232ecc6b5895ae3e0cc313cd5a64746
zerpaerik <zerpaerik@gmail.com>
2021-04-01 09:07:26 -0400

    ddd

--- resources/views/reportes/ticket_atencion_ver.blade.php
+++ resources/views/reportes/ticket_atencion_ver.blade.php
@@ -131,6 +131,101 @@
 	<center><p style="font-size: 15px;">COMUNICADO</p></center>
 	<p style="text-align: justify;font-size: 15px;">Estimado cliente se informa, que todo estudio que quede pendiente de su realizaciòn <strong>tiene un plazo no mayor a 30 dias,</strong>contando desde la fecha de su cancelaciòn, <strong>pasado este tiempo quedarà como anulado dicho estudio</strong>. Asi mismo las <strong>consultas de reevaluaciòn tienen un plazo de 15 dias,</strong> pasado este tiempo el paciente deberà cancelar por su consulta.</p>
 
+	<div class="" style="font-size: 35px; text-align: center;margin-bottom: 15px;">
+		<img src="/var/www/html/syspro/public/img/image.png"  style="width: 40%;"/>
+	</div>
+
+    <div  style="font-size: 15px; text-align: center;margin-bottom:-60px;margin-top: -30px;">
+    @if(Session::get('sedeName') == 'ZARATE')
+
+		<p><strong>SYSMEDIC PERU SAC - {{Session::get('sedeName')}}</strong></p>
+		<p style="margin-top: -20px;"><strong>RUC: 20606283980</strong></p>
+    @else
+    <p><strong>SYSMEDIC PERU SAC - {{Session::get('sedeName')}}</strong></p>
+		<p style="margin-top: -20px;"><strong>RUC: 20606283980</strong></p>
+    @endif
+		@if(Session::get('sedeName') == 'INDEPENDENCIA')
+		<p><strong>Teléfono: 5265711</strong></p>
+		<p><strong>WhatsApp: 940309507</strong></p>
+    @elseif(Session::get('sedeName') == 'ZARATE')
+    <p><strong>Teléfono: 7820512</strong></p>
+		<p><strong>WhatsApp: 924520026</strong></p>
+		@else
+		<p><strong>Teléfono: 5390547</strong></p>
+		<p><strong>WhatsApp: 940309506</strong></p>
+		@endif
+
+
+
+    <p style="margin-top: -20px;"><strong>________________________________________________________</strong>  </p>
+    <p style="margin-top: -15px;font-size: 14px;text-align: center;"><strong>NÚMERO DE RECIBO ELECTRÓNICO:0000{{ $ticket->id}}</strong></p>
+    <p style="margin-top: -30px;"><strong>________________________________________________________</strong>  </p>
+
+    <p style="margin-left: -80px;font-size: 14px;"><strong>FECHA:</strong> {{$ticket->created_at}} </p>
+
+	
+
+	</div>
+    <br><br>
+    <div  style="font-size: 15px; text-align: left;margin-bottom:-20px;margin-top: 20px;">
+		<p style="margin-left: -40px;"><strong>PACIENTE:</strong> {{ $ticket->nombres}},{{ $ticket->apellidos}}</p>
+    <p style="margin-left: -40px;margin-top: -10px;"><strong>DNI:</strong> {{ $ticket->dni}}</p>
+    <p style="margin-left: -40px;"><strong>ORIGEN:</strong> </p>
+
+	
+	</div>
+  <br><br>
+
+  
+  <table width="100%" class="table-main">
+      <thead>
+        <tr>
+          <th style="font-size: 15px"><center>Detalle.<center></th>
+          <th style="font-size: 15px"><center>Monto.<center></th>
+        </tr>
+      </thead>
+      <tbody>
+          <tr>
+            <td style="font-size: 15px; line-height: 30px;" align="center">{{ $ticket->detalle}}</td>
+            <td style="font-size: 15px; line-height: 30px;" align="center">{{ $ticket->monto}}</td>
+          </tr>
+      </tbody>
+    </table>
+
+
+
+
+    <br>
+
+    <table width="100%">
+      <tbody>
+        <tr>
+          <td style="width: 100%;">
+            <table width="100%">
+              <tbody>
+                   
+
+                    <tr>
+                      <td align="left" style="font-size: 15px;margin-left:150px;">TOTAL</td>
+                      <td align="right" style="font-size: 15px;margin-left:150px;">{{$ticket->monto }}</td>
+                    </tr>
+					<tr>
+                      <td align="left" style="font-size: 15px;margin-left:150px;">PAGADO</td>
+                      <td align="right" style="font-size: 15px;margin-left:150px;">{{ $ticket->abono}}</td>
+                    </tr>
+					<tr>
+                      <td align="left" style="font-size: 15px;margin-left:150px;">RESTA</td>
+                      <td align="right" style="font-size: 15px;margin-left:150px;">{{ $ticket->pendiente}}</td>
+                    </tr>
+              </tbody>
+            </table>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+
+	<br><br>
+
 	
 
     
